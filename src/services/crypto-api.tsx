1f248960d8cefc7ec2a868d04224d3de247b94ad
Tohir <TOXIR_DEV>
2022-07-25 22:11:43 +0600

    I creates some pages and apis

---
+++ src/services/crypto-api.tsx
@@ -0,0 +1,24 @@
+/** @format */
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     "tiers[0]": "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//   },
+// };
