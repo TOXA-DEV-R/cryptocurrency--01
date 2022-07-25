733d41de2a5f6e5a72eddb9781ed44819b7b1db1
Tohir <TOXIR_DEV>
2022-07-25 23:36:56 +0600

    api

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import { BrowserRouter as Router } from "react-router-dom";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 const root: HTMLElement | null = document.getElementById("root");
 const dom = ReactDOM.createRoot(root as HTMLElement);
 
 dom.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
